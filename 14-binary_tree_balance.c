cf7914105237558e1fb7fa6dcb76da3e10a73094
abraham-ship <bettabraham11@gmail.com>
2023-08-03 01:30:08 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,13 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-                return (0);
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
-        return ((left_height > right_height) ? (left_height + 1) : (right_height + 1));
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return ((left_height > right_height) ? (left_height + 1)
+			: (right_height + 1));
 }
 
 /**
